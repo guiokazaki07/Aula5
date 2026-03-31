36f2c59bbe2dc1e689095b37ac199e3c816eea90
guiokazaki07 <guiokazaki07@gmail.com>
2026-03-30 21:18:35 -0300

    Delete Aula 5 directory

--- Aula 5/pilula5.py
+++
@@ -1,9 +0,0 @@
-def notasLiberadas(valor):
-    for nota in (100,50,20,10):
-        qtd = valor // nota
-        valor = valor % nota
-        if qtd > 0:
-            print(f'{qtd} notas de {nota}')
-            
-valor = int(input('Digite o valor: '))
-notasLiberadas(valor)
